6bb94c5cdcfd330cd4d64a0bbbe78305de4ee725
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-10-06 15:30:09 +0200

    Update index.jsx

--- src/components/Links/index.jsx
+++ src/components/Links/index.jsx
@@ -6,18 +6,15 @@ class Links extends React.Component {
   render() {
     const author = this.props.data
     const links = {
-      telegram: author.elements.telegram.value,
       twitter: author.elements.twitter.value,
-      github: author.elements.github.value,
-      vk: author.elements.vk.value,
       // rss: author.elements.rss.value,
-      email: author.elements.email.value,
+      email: author.elements.email.value
     }
 
     return (
       <div className="links">
-        <ul className="links__list">
-          <li className="links__list-item" data-kontent-element-codename="twitter">
+        {/* <ul className="links__list">
+          <li className="links__list-item" data-kontent-element-codename="twitter"> */}
             <a
               href={`https://www.twitter.com/${links.twitter}`}
               target="_blank"
@@ -25,38 +22,15 @@ class Links extends React.Component {
             >
               <i className="icon-twitter" />
             </a>
-          </li>
-          <li className="links__list-item" data-kontent-element-codename="github">
-            <a
-              href={`https://www.github.com/${links.github}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="icon-github" />
-            </a>
-          </li>
-          <li className="links__list-item" data-kontent-element-codename="vk">
-            <a
-              href={`https://www.vk.com/${links.vk}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="icon-vkontakte" />
-            </a>
-          </li>
+          {/* </li>
         </ul>
         <ul className="links__list">
-          <li className="links__list-item" data-kontent-element-codename="email">
+          <li className="links__list-item" data-kontent-element-codename="email"> */}
             <a href={`mailto:${links.email}`}>
               <i className="icon-mail" />
             </a>
-          </li>
-          <li className="links__list-item" data-kontent-element-codename="telegram">
-            <a href={`telegram:${links.telegram}`}>
-              <i className="icon-paper-plane" />
-            </a>
-          </li>
-        </ul>
+          {/* </li>
+        </ul> */}
       </div>
     )
   }
